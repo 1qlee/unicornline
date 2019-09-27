4d5e0828a6d8537ecd481f4acb9fb9e7e3b80b48
Wonkyu <wonq33@gmail.com>
2019-09-27 12:56:35 -0400

    Env displaced

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         // You can find your read-only API token under the Settings > API tokens
         // section of your administrative area:
-        apiToken: `9af41e49b7a7d7994b6cddbe3f65f8`,
+        apiToken: process.env.DATO_API_TOKEN,
 
         // If you are working on development/staging environment, you might want to
         // preview the latest version of records instead of the published one:
